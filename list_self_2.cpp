4556c4e26082564dc78bdc85c54fcadcbe03fd93
“yvonne” <3232754308@qq.com>
2023-04-10 15:46:09 +0800

    顺序表

--- list_self_2.cpp
+++ list_self_2.cpp
@@ -1,135 +1,133 @@
+//在非主函数的函数里尽量不要出现输出，因为这是主函数的事情
 #include<stdio.h>
 #include<stdlib.h>
 #include<iostream>
 using namespace std;
+
+//存储结构
 typedef struct
 {
     int *elem;
     int length;
-}Sqlist;
+}SqList;
 
-/* 菜单 */
-void Menu()
+//初始化
+//步骤：为顺序表L动态分配一个预定义大小的数组空间，使elem指向这段空间的基地址，表的当前长度设为0
+int IniitList (SqList &L)//引用：改变L;
 {
-    cout << "**********菜单**********" << endl;
-    cout << "1.初始化" << endl;
-    cout << "2.取值" << endl;
-    cout << "3.查找" << endl;
-    cout << "4.插入" << endl;
-    cout << "5.删除" << endl;
-    cout << "6.遍历" << endl;
-    cout << "7.销毁" << endl;
-    cout << "8.清空" << endl;
-    cout << "9.退出" << endl;
-    cout << "************************" << endl;
-}
-
-/* 初始化 */
-void InitList(Sqlist &L)
-{
-    L.elem = (int *)malloc(100 * sizeof(int));
+    L.elem = new int[100];//分配一个数组空间
     if (!L.elem)
-        exit(-2);
+        exit(-2);//这个x是返回给操作系统（包括UNIX,Linux,和MS DOS）的，以供其他程序使用。
+        //0是正常退出，其他均是异常，overflow(-2)
     L.length = 0;
-    cout << "初始化成功" << endl;
-}
-/* 销毁 */
-void DestroyList(Sqlist &L)
-{
-    free(L.elem);
-    L.length = 0;
-    cout << "销毁成功" << endl;
-}
-
-/* 取值 */
-
-/* 查找 */
-
-/* 插入 */
-int Insert( Sqlist &L, int X, int i ) //L 表 X 元素 i 插入位置
-{ 
-    int j;
-    if (L.length == 100) /* 顺序线性表已经满 */
-    {//一开始忘记大括号了！！！
-        cout << "顺序线性表已经满" << endl;
-        return 0;
-    }
-    if (i < 1 || i > L.length+1) /* 当i小于1或者大于表长加1时 */
-    {
-        cout << "插入位置不合法" << endl;
-        return 0;
-    }
-    if (i <= L.length) /* 若插入数据位置不在表尾 */
-    {
-        for (j = L.length-1; j >= i-1; j--)
-            L.elem[j+1] = L.elem[j]; /* 将要插入位置后数据元素向后移动一位 */
-    }
-    L.elem[i-1] = X; /* 将新元素插入 */
-    L.length++;
     return 1;
 }
 
-/* 删除 */
+//取值
+//步骤：判断位置是否合理，合理将第i个数据元素L.elem[i-1]赋给参数e,通过e返回第i个元素的传值
+int GetElem(SqList L,int i,int &e)
+{
+    if(i<1 || i>L.length)
+        return 0;
+    e = L.elem[i-1];
+    return 1;
+}
 
-/* 遍历 */
+//查找
+//步骤：从第一个元素起，和e比较，找到相同元素，返回序号，查找失败返回0
+int LocateElem(SqList L,int e)
+{
+    for(int i=0;i<=L.length;i++)
+    {
+        if (L.elem[i]==e)
+            return i+1;
+    }
+    return 0;
+}
 
-/*销毁*/
+//插入
+//步骤：判断插入位置是否合法，存储空间是否已满，元素后移，空出第i个位置，插入，表长加一
+int ListInsert(SqList &L,int i,int e)
+{
+    if (i<1||i>L.length+1)
+        return 0;
+    if(L.length==100)//还可以改，加内存
+        return 0;
+    for(int j=L.length-1;j>=i-1;j--)
+    {
+        L.elem[j+1]=L.elem[j];//注意循环主语
+    }
+    L.elem[i-1]==e;
+    ++L.length;
+    return 1;
+}
 
-/*清空*/
+//删除
+//步骤：判断是否合法，向前移动，表长减1
+int ListDelete(SqList &L,int i)
+{
+    if(i<1||i>L.length)
+        return 0;
+    //for(int j=L.length-1;j>=1;j--)
+    for (int j=i;j<=L.length-1;j++)//加也可以
+        L.elem[j-1]=L.elem[j];
+    --L.length;
+    return 1;
+}
+
+//遍历
+//步骤：从第一个元素开始，依次输出
+int ListTraverse(SqList L)
+{
+    for(int i=0;i<L.length;i++)
+        cout<<L.elem[i]<<endl;
+    return 1;
+}
 
 int main()
 {
-    int input = 0;
-    do
-    {
-        Menu();
-        cin >> input;
-        switch (input)
-        {
-        case 1:
-            /* 初始化 */
-            //进行初始化考虑到的问题：1是否有必要把初始化放入循环中？2初始化两次会造成什么后果：比如第一次的初始化列表内容是否还可以找到，怎么找
-            //如果要消除或销毁第一次初始化后再进行第二次初始化应该怎么做
-            //现在可以确定下面的L是可以更改的
-            //调试可知再次初始化后，第一次初始化的内容没有了,不确定是否被销毁
-            Sqlist L;
-            InitList(L);
-            break;
-        case 2:
-            /* 取值 */
-            break;
-        case 3:
-            /* 查找 */
-            break;
-        case 4:
-            /* 插入 */
-            printf ("请输入插入的元素和位置：");
-            int X, i;
-            scanf ("%d %d", &X, &i);            
-            Insert(L, X, i);
-            break;
-        case 5:
-            /* 删除 */
-            break;
-        case 6:
-            /* 遍历 */
-            break;
-        case 7:
-            /* 销毁 */
-            break;
-        case 8:
-            /* 清空 */
-            break;
-        default:
-            break;
-        }
-    } while (input != 9);
+    SqList L;
+    int e;
+    int i;
+
+    //初始化
+    IniitList(L);
+
+    //赋值
+    for(int i=0;i<10;i++)
+        L.elem[i]=i+1;
+
+    //遍历
+    ListTraverse(L);
+
+    //查找
+    cout<<"请输入要查找的元素："<<endl;
+    cin>>e;
+    i=LocateElem(L,e);
+    if(i)
+        cout<<"该元素的位置是："<<i<<endl;
+    else
+        cout<<"查找失败"<<endl;
+
+    //删除
+    cout<<"请输入要删除的元素的位置："<<endl;
+    cin>>i;
+    ListDelete(L,i);
+    ListTraverse(L);
+
+    //插入：空列表插不了
+    cout<<"请输入要插入的元素的位置："<<endl;
+    cin>>i;
+    cout<<"请输入要插入的元素："<<endl;
+    cin>>e;
+    ListInsert(L,i,e);
+    ListTraverse(L);
+
+    //取值
+    cout<<"请输入要取值的元素的位置："<<endl;
+    cin>>i;
+    GetElem(L,i,e);
+    cout<<"该元素的值是："<<e<<endl;
+
     return 0;
-//问题：如果一开始想一次性输入一些数据怎么办，插入不能改，之后的小部分修改需要用到
-//老师讲链表的时候讲了最开始的创建 - 已建立创建 - 问题解决
-//一开始忘记大括号了！！！(python) lyt- try-except用法
-//初始化问题(初始化程序已提取出)——即单个列表 void类型 
-//错误：重新输入 while ,用if会直接退出
- //问题：销毁后，再次使用会出现问题，因为没有初始化
-//既然单个列表，销毁后没必要再使用程序，即退出程序
 }
